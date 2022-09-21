99fd734e5b8138d02d48cd9afd60e28b266c19ea
kennywindis <kennyphilip100@gmail.com>
2022-09-21 16:55:42 -0700

    ielf

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,11 +5,11 @@ BUZZ = "Buzz"
 
 def fizzbuzz():
     for number in range(1, 101):
-        if (number % 3 and number % 5):
-            print("%s%s" % (FIZZBUZZ), end=' ')
-        elif (number % 3):
+        if (number % 3 == 0 and number % 5 == 0):
+            print("%s%s" % (FIZZ, BUZZ), end=' ')
+        elif (number % 3 == 0):
             print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
+        elif (number % 5 == 0):
             print("%s" % (BUZZ), end=' ')
         else:
             print("%d" % (number), end=' ')
