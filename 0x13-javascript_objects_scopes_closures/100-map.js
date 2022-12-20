8f4c63f46c375cc57e48e3b63a23200afedb5130
kennywindis <kennyphilip100@gmail.com>
2022-12-20 15:53:14 -0800

    js2

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = require('./100-data').list;
+const array = require('./100-data.js').list;
 const newArray = array.map((x, index) => x * index);
 console.log(array);
 console.log(newArray);
