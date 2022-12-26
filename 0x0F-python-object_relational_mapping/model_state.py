2fbc1de707ad9a48e3c8e186a57ad4b5d3fff982
kennywindis <kennyphilip100@gmail.com>
2022-12-25 17:35:45 -0800

    py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+created by kehinde
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    state class for use with sqlalchemy inherits from sqlalchemy
+    declarative_base
+    Attributes:
+        Base (class)
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
