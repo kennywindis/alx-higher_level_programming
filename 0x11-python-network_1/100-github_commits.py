6efd87ea8fd345c549045607a773a86c28e20934
kennywindis <kennyphilip100@gmail.com>
2023-01-17 14:58:07 -0800

    ntwk

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
-from requests import get
-from sys import argv
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
 
 
 if __name__ == "__main__":
-    username = argv[2]
-    repo = argv[1]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
-    response = get(url)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
     try:
-        objects = response.json()
-        for i, obj in enumerate(objects):
-            print('{}: {}'.format(obj.get('sha'),
-                                  obj.get('commit').get('author')
-                                  .get('name')))
-            if i == 9:
-                break
-    except ValueError:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
         pass
