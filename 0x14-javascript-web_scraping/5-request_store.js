14bdd554291c73f210c36f6e46f14a7421bf6d18
kennywindis <kennyphilip100@gmail.com>
2023-02-09 05:13:03 -0800

    jsws

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const file = process.argv[3];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    fs.writeFile(file, body, 'utf8', function (error, data) {
-      if (error) {
-        console.error(error);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
 
