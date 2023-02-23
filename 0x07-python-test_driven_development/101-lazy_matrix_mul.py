4b15f073246772156edc6e67136fea24d5b879c8
kennywindis <kennyphilip100@gmail.com>
2023-02-23 14:45:59 -0800

    tdd

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 from numpy import matmul
 
-
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiply two matrix
     Args:
-        m_a (list): First matrix
-        m_b (list): second matrix
+        m_a (lists/floats): First matrix
+        m_b (lists/floats): second matrix
     Returns:
         The result of multiply m_a by m_b
     """
